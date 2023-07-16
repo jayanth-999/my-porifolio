b55c56e772b87cfdb319279a7733818565ed9320
jayanth-999 <jayanthnuggu@gmail.com>
2023-07-16 21:36:05 +0530

    completed Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 
+
 const Header = () => {
-  return <div className='bg-pink-400'>
-    <div>
-      <h1>Potifolio</h1>
+  return <header className='bg-pink-500' py-8>
+    <div className='container mx-auto'>
+      <div className='flex justify-between items-center'>
+        <a href='#'>
+          {/* <img src={Logo} alt=''/> */}
+          <h2 className='text-sky-50 shadow-accent'>JAYANTH NUGGU</h2>
+        </a>
+        <button className='btn btn-sm my-0.5'>Contact Me</button>
+      </div>
     </div>
-  </div>;
+  </header>;
 };
 
 export default Header;
