9eb7ea94b4214b3ec9e9f31a53ebf70658b352e3
jayanth-999 <jayanthnuggu@gmail.com>
2023-07-16 20:26:09 +0530

    completed nav bar

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return <div className='bg-pink-400'>
+    <div>
+      <h1>Potifolio</h1>
+    </div>
+  </div>;
+};
+
+export default Header;
