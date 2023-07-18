4078a106d1d62d38f22d8254058e3b931cb38dbd
jayanth-999 <jayanthnuggu@gmail.com>
2023-07-18 21:56:52 +0530

    added baner elements

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Image from '../assets/avatar.jpg';
+import { FaGithub , FaLinkedin } from 'react-icons/fa';
+import { TypeAnimationProps } from 'react-type-animation';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants'
 
 const Banner = () => {
-  return <div className='section' id='home'>Banner</div>;
+  return <section className='section' id='home'>
+    <div className='container mx-auto'>
+      <div className='flex'>
+        <div>
+          <h1>
+            JAYANTH <span>NUGGU</span>
+            </h1>
+        </div>
+        <div>
+          <img className='rounded-full' src={Image} alt=''/>
+        </div>
+      </div>
+    </div>
+    </section>;
 };
 
 export default Banner;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 
 const Header = () => {
-  return <header className='bg-pink-500' py-8>
+  return <header className='py-4'>
     <div className='container mx-auto'>
-      <div className='flex justify-between items-center'>
+      <div className='flex justify-around items-center'>
         <a href='#'>
           {/* <img src={Logo} alt=''/> */}
-          <h2 className='text-sky-50 shadow-accent'>JAYANTH NUGGU</h2>
+          <h2 className='text-indigo-50'>JAYANTH NUGGU</h2>
         </a>
-        <button className='btn btn-sm my-0.5'>Contact Me</button>
+        <button className='btn btn-sm'>Contact Me</button>
       </div>
     </div>
   </header>;
