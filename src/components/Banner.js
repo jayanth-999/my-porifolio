746179ca24cbd2b1677c15dbff7d6d612aab0d86
jayanth-999 <jayanthnuggu@gmail.com>
2023-07-22 20:22:42 +0530

    commpleted banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,25 +1,56 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import Image from '../assets/avatar.jpg';
-import { FaGithub , FaLinkedin } from 'react-icons/fa';
-import { TypeAnimationProps } from 'react-type-animation';
-import { motion } from 'framer-motion';
-import { fadeIn } from '../variants'
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { TypeAnimation } from 'react-type-animation';
+
 
 const Banner = () => {
-  return <section className='section' id='home'>
+  return (<section className='min-h-[85vh] lg:min-h-[78wh] flex items-center' id='home'>
     <div className='container mx-auto'>
-      <div className='flex'>
-        <div>
-          <h1>
+      <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
+        <div className='flex-1 text-center font-secondary lg:text-left'>
+          <h1 className='text-[55px] font-bold leading-[0.8] 1g:text-[110px]'>
             JAYANTH <span>NUGGU</span>
-            </h1>
+          </h1>
+          <div className='md-6 text-[36px] lg:text-[40px] font-secondary font-semibold uppercase leading-[1]'>
+            <span className='text-white mr-2'>I am a </span>
+            <TypeAnimation
+              sequence={[
+                'Developer',
+                2000,
+                'DevOps engineer',
+                2000,
+                'Programmer',
+                2000,
+              ]}
+              speedp={50}
+              className='text-accent'
+              wrapper='span'
+              repeat={Infinity}
+            />
+          </div>
+          <p className='md-8 max-w-lg mx-auto lg:mx-0'>
+            I am enthusiastic about learning new things and exploring new technologies.
+            I particularly enjoy working collaboratively with people.
+          </p>
+          <div className='flex max-w-max gap-x-6 items-center mb-6 mx-auto lg:mx-0'>
+            <button className='btn btn-lg'>Contact Me</button>
+            <a href='#' className='text-gradient btn-link'>
+              My Portifolio
+            </a>
+          </div>
+          <div className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0 '>
+            <a href='#'><FaGithub/></a>
+            <a href='#'><FaLinkedin/></a>
+          </div>
         </div>
         <div>
-          <img className='rounded-full' src={Image} alt=''/>
+          <img className='rounded-full' src={Image} alt='' />
         </div>
       </div>
     </div>
-    </section>;
+  </section>)
 };
 
 export default Banner;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import React from 'react';
 
 
 const Header = () => {
-  return <header className='py-4'>
+  return <header className='py-8'>
     <div className='container mx-auto'>
       <div className='flex justify-around items-center'>
         <a href='#'>
           {/* <img src={Logo} alt=''/> */}
-          <h2 className='text-indigo-50'>JAYANTH NUGGU</h2>
+          <h2 className='text-indigo-50 text-[30px] font-bold'>JAYANTH NUGGU</h2>
         </a>
         <button className='btn btn-sm'>Contact Me</button>
       </div>
